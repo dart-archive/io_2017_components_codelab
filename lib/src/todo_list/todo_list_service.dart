4849ae6961bb2cc6635fddc24e3f02ac1b65dddf
Kathy Walrath <kathyw@google.com>
2017-06-15 17:29:49 -0700

    Update 1_basic to stagehand 1.1.4

---
+++ lib/src/todo_list/todo_list_service.dart
@@ -0,0 +1,15 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:angular2/core.dart';
+
+/// Mock service emulating access to a to-do list stored on a server.
+@Injectable()
+class TodoListService {
+  List<String> mockTodoList = <String>[];
+
+  Future<List<String>> getTodoList() async => mockTodoList;
+}
